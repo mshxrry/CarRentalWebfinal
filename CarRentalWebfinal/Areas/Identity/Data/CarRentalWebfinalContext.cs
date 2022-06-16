f403a0cc846e2a476729d322fcc0ff0efb78149f
ac149815 <ac149815@8CG8163L28>
2022-06-17 10:09:47 +1200

    Add project files.

---
+++ CarRentalWebfinal/Areas/Identity/Data/CarRentalWebfinalContext.cs
@@ -0,0 +1,32 @@
+﻿using CarRentalWebfinal.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using CarRentalWebfinal.Models;
+using CarRentalWebfinal.Models;
+
+namespace CarRentalWebfinal.Areas.Identity.Data;
+
+public class CarRentalWebfinalContext : IdentityDbContext<CarRentalWebfinalUser>
+{
+    public CarRentalWebfinalContext(DbContextOptions<CarRentalWebfinalContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+
+    public DbSet<CarRentalWebfinal.Models.Customer>? Customer { get; set; }
+
+    public DbSet<CarRentalWebfinal.Models.Reservation>? Reservation { get; set; }
+
+    public DbSet<CarRentalWebfinal.Models.Location>? Location { get; set; }
+
+    public DbSet<CarRentalWebfinal.Models.Vehicle>? Vehicle { get; set; }  
+}
---
+++ CarRentalWebfinal/Areas/Identity/Data/CarRentalWebfinalUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace CarRentalWebfinal.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the CarRentalWebfinalUser class
+public class CarRentalWebfinalUser : IdentityUser
+{
+}
+
