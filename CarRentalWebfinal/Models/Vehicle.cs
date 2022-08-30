3bd7b881893e783900b37149e6281f324e2408fa
Muhammad Shaharyar <ac149815@avcol.school.nz>
2022-08-30 14:14:16 +1200

    added data anotations and changed the indexes

--- CarRentalWebfinal/Models/Vehicle.cs
+++ CarRentalWebfinal/Models/Vehicle.cs
@@ -1,14 +1,19 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace CarRentalWebfinal.Models
 {
     public class Vehicle
     {
         public int VehicleId { get; set; }
-
+        [StringLength(20, ErrorMessage = "The brand name cannot exceed 20 characters")]
         public string Brand { get; set; }
+        [StringLength(20, ErrorMessage = "The model name cannot exceed 20 characters")]
         public string Model { get; set; }
+        [StringLength(20, ErrorMessage = "The type cannot exceed 20 characters")]
         public string Type { get; set; }
+
+        [StringLength(20, ErrorMessage = "The transmission cannot exceed 20 characters")]
         public string Transmission { get; set; }
         public int ModelNumber { get; set; }
 
